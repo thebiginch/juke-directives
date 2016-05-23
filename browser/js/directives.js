3933aec50be7ebe6fe0b3241bb07ca4de63e9c87
Zachary Levine <zrlevine@gmail.com>
2016-05-23 15:22:42 -0400

    fixed sidebar directive

---
+++ browser/js/directives.js
@@ -0,0 +1,40 @@
+juke.directive('sidebar', function() {
+
+    return {
+        restrict: 'E',
+        templateUrl: '/js/sidebar/sidebar.html',
+        link: function(scope, element, attr) {},
+        scope: {},
+    }
+
+});
+
+juke.directive('player', function() {
+    
+    return {
+        restrict: 'E',
+        templateUrl: '/js/player/player.html',
+        link: function(scope) {
+
+        },
+    }
+
+})
+
+juke.directive('albumList', function(){
+
+	return {
+		restrict: 'E',
+		templateUrl: '/js/album/templates/albums.html',
+		link: function(scope){
+
+
+		},
+		scope: {
+			albums: '='
+		}
+
+	}
+
+
+});
